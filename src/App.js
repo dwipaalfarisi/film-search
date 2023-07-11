c3cc6ac107b9825403187d51132ace0e862ae95e
dwipaalfarisi <dwipa.alfarisi@gmail.com>
2023-07-11 22:56:21 +0700

    add base film catalogue

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getFilmList } from "./api";
 
-function App() {
+const App = () => {
+  const [popularFilms, setPopularFilms] = useState([]);
+
+  useEffect(() => {
+    getFilmList().then((result) => setPopularFilms(result));
+  }, []);
+
+  const PopularFilmList = () => {
+    return popularFilms.map((film, i) => {
+      return (
+        <div className="Film-wrapper" key={i}>
+          <div className="Film-title">{film.title}</div>
+          <img
+            className="Film-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}${film.poster_path}`}
+            alt="film poster"
+          />
+          <div className="Film-date">{film.release_date}</div>
+          <div className="Film-rate">{film.average_vote}</div>
+        </div>
+      );
+    });
+  };
+
+  const search = (q) => {
+    console.log({ q });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>FILMS</h1>
+        <input
+          placeholder="Find films..."
+          className="Film-search"
+          onChange={({ target }) => search(target.value)}
+        />
+        <div className="Film-container">
+          <PopularFilmList />
+        </div>
       </header>
     </div>
   );
-}
+};
 
 export default App;
